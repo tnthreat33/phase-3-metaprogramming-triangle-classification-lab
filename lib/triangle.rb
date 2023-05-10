7ac99fe47a49bf2bb69b6a84340137fa4b66ee44
Rachel Hamby <tnthreat33@gmail.com>
2023-05-10 19:09:07 -0400

    commit tree 2cf23de296cb16c41bd0c6cc3781ec3e001dc75a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+
 class Triangle
-  # write code here
+
+  attr_reader :s1, :s2, :s3 
+
+  def initialize (s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind 
+    validate 
+
+    if s1 === s2 && s1 == s3
+      :equilateral
+   
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      :isosceles
+    
+    else 
+      :scalene
+
+  end 
+end
+
+  def side_check
+    s1 >0 && s2> 0 && s3 > 0 
+  end 
+
+  def sum_check
+    s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1
+  end 
+
+  def validate
+    raise TriangleError unless side_check && sum_check 
+  end 
+  
+
+  class TriangleError < StandardError
+  end
 end
